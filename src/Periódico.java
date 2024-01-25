8d63061abdbec44f42ced6b757e872ca128eb32c
Guillermo1KDAM <guigommor@alu.edu.gva.es>
2024-01-25 20:22:47 +0100

    ToString Pruebas

--- src/Periódico.java
+++ src/Periódico.java
@@ -1,5 +1,8 @@
 public class Periódico {
     public static void main(String[] args) {
-        int numSemanas;
+
+        int numSemanas=1;
+        System.out.println(String.format("%-10.25s %-10.25s %-10.25s %-10.25s %-10.25s %-10.25s %-10.25s", "Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"));
+
     }
 }
--- src/VentasSemanales.java
+++ src/VentasSemanales.java
@@ -11,8 +11,26 @@ public class VentasSemanales {
         Arrays.fill(seguimiento, 0);
     }
 
-    public void guardaVenta(int semana, int dia, int valor){
-        seguimiento[semana][dia] = valor;
+    public void guardaVenta(int semana, int dia, int valor) {
+        if (semana < 1 && dia >= 0 && dia < 7) {
+            seguimiento[semana][dia] = valor;
+        } else {
+            System.out.println("Esta fuera de rango");
+        }
+    }
+    public void inicializaVenta(){
+        Arrays.fill(seguimiento, 0);
     }
 
+    @Override
+    public String toString() {
+        StringBuilder tabla= new StringBuilder("Semana");
+        tabla.append(String.format("%-10.25s %-10.25s %-10.25s %-10.25s %-10.25s %-10.25s %-10.25s %-10.25s","Alumno", "Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo\n"));
+
+        for (int i=0; i<numSemanas; i++){
+            tabla.append(String.format("%10s"));
+        }
+        return tabla.toString();
+    }
 }
+
